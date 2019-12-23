344bf90a73874d93a38402bd7852402a293c77e4
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-12-23 17:28:38 -0500

    build: play sound with button"

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
 import '../App.css';
+import shortBeep from '../shortBeep.mp3';
+// import UIfx from 'uifx';
+
+// const beep = new UIfx(
+//   shortBeep,
+//   {
+//     volume: 0.4,
+//     throttleMs: 100
+//   }
+// );
+
+const beep = new Audio(shortBeep)
+const start = () => {
+  beep.play()
+}
 
 class Button extends Component {
   constructor(props) {
@@ -21,7 +36,8 @@ class Button extends Component {
   render() {
     return (
       <div className="buttonContainer">
-        <div className="switchButton" onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp}></div>
+        {/* <div className="switchButton" onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp}></div> */}
+        <button onClick={start}>Let's Beep</button>
       </div>
     )
   }
