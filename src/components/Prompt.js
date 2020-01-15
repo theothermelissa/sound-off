e2045f7735b64f2b9fe7145738b1971f46ee2ccd
Melissa Morgan <melissapearlmorgan@gmail.com>
2020-01-15 17:18:27 -0500

    build: add "completed" class to completed elements and change to black

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -5,26 +5,25 @@ import '../App.css';
 import { act } from 'react-dom/test-utils';
 
 const Prompt = ({ char, resetLastSignal, activeCharacterIndex, lastSignalReceived, position, completePrompt, totalCharacters }) => {
-  const activeStatus = (position === activeCharacterIndex) ? true : false;
-  console.log("Is Prompt index ", position, " active? ", activeStatus)
 
   const onCompleteSequence = () => {
-    console.log("Completed prompt: ", position)
     completePrompt(position);
   };
 
   return (
     <div className="promptContainer">
       <div className="letter">{char}</div>
-      <Sequence 
-        char={char}
-        position={position}
-        resetLastSignal={resetLastSignal}
-        lastSignalReceived={lastSignalReceived}
-        completeSequence={onCompleteSequence}
-        activeCharacterIndex={activeCharacterIndex}
-        totalCharacters={totalCharacters}
-      />
+      <div className="sequence">
+        <Sequence 
+          char={char}
+          position={position}
+          resetLastSignal={resetLastSignal}
+          lastSignalReceived={lastSignalReceived}
+          completeSequence={onCompleteSequence}
+          activeCharacterIndex={activeCharacterIndex}
+          totalCharacters={totalCharacters}
+        />
+      </div>
     </div>
   );
 
--- src/components/Sequence.js
+++ src/components/Sequence.js
@@ -1,63 +1,72 @@
 import React, { useState } from 'react';
-// import alphabet from '../assets/codeTranslationKey.js';
+import alphabet from '../assets/codeTranslationKey.js';
 import '../App.css';
-// import signalElements from '../assets/signalElements';
+import signalElements from '../assets/signalElements';
 
 
-// const morseElementSequence = alphabet[char.toLowerCase()]["sequence"];
-// const checkSignal = () => {
-  // }
-  
-  // const updateCount = () => {
-    //   let newCount = count + 1;
+
+// const updateCount = () => {
+  //   let newCount = count + 1;
   //   if (charStatus === "active") {
     //     if (newCount > 1) {
-  //       completeSignal(charPosition);
-  //       setCount("complete");
-  //     } else {
-    //       setCount(newCount)
-    //     };
-    //   };
-    //   }
+      //       completeSignal(charPosition);
+      //       setCount("complete");
+      //     } else {
+        //       setCount(newCount)
+        //     };
+        //   };
+        //   }
+        
+        
+        // const Sequence = ({ char, position, activeCharacterIndex, completeCharacterSignal }) => {
+          //   const [activeSignalIndex, setActiveSignalIndex] = useState[0];
+          //   const [color, setColor] = useState["green"]
+          //   const codeSequence = alphabet[char.toLowerCase()]["sequence"];
+          //   const totalCharacterSignals = codeSequence.length;
+          //   const activeStatus = (position === activeCharacterIndex) ? "active" : "inactive";
+          
+          // const checkSignal = () => {
+            //   }
+            
+// .map((element, index) => { 
+  //     return (
+    //       <div 
+    //         activeElementIndex={activeElementIndex}
+    //         charStatus={charStatus}
+    //         className={element.id}
+    //         key={element.id + char + index}>
+    //       </div>
+    //     )
+    //   })
+    // };
 
-    // .map((element, index) => { 
-      //     return (
-        //       <div 
-//         activeElementIndex={activeElementIndex}
-//         charStatus={charStatus}
-//         className={element.id}
-//         key={element.id + char + index}>
-//       </div>
-//     )
-//   })
-// };
-
-// const Sequence = ({ char, position, activeCharacterIndex, completeCharacterSignal }) => {
-  //   const [activeSignalIndex, setActiveSignalIndex] = useState[0];
-  //   const [color, setColor] = useState["green"]
-  //   const codeSequence = alphabet[char.toLowerCase()]["sequence"];
-  //   const totalCharacterSignals = codeSequence.length;
-  //   const activeStatus = (position === activeCharacterIndex) ? "active" : "inactive";
-  
-  
 const Sequence = ({ char, position, activeCharacterIndex, completeSequence, totalCharacters }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   // console.log("Sequence position: ", position)
-
+  const morseElementSequence = alphabet[char.toLowerCase()]["sequence"];
+  
   const clickButton = () => {
+    console.log("The ", {char}, " button has been clicked.")
     let newIndex = currentIndex +1;
     if (newIndex < totalCharacters) {
-      console.log("The", char, " button has been clicked.")
-      console.log("Sequence: activeCharacterIndex: ", activeCharacterIndex)
       setCurrentIndex(currentIndex + 1);
       completeSequence();
     }
   }
 
+  const isComplete = (position < activeCharacterIndex);
+
+  // className={`post-wrapper ${this.state.loading ? 'post-wrapper--loading' : ''}`}
+
   return (
-    <button onClick={clickButton}>
-    {char}
-  </button>
+    morseElementSequence.map((codeSignal, index) => {
+      return (
+        <div className={`${codeSignal.id} ${isComplete ? 'completed' : ''}`} key={char + codeSignal.id + index} onClick={clickButton} />
+      )
+    })
+  //   <button onClick={clickButton}>
+  //   {char}
+  // </button>
 )
 };
   
