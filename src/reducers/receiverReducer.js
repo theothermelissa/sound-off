70dd8cee94896c335a47a0adb17a0e1c83b474dc
Melissa Morgan <melissapearlmorgan@gmail.com>
2020-02-02 10:00:26 -0500

    refactor: modified receiverReducer per style guide

--- src/reducers/receiverReducer.js
+++ src/reducers/receiverReducer.js
@@ -1,58 +1,58 @@
 const receiverReducer = (state, action) => {
   const newErrorCount = state.totalErrors + 1;
   switch (action.type) {
-    case "invalidSignal":
+    case 'invalidSignal':
       return {
         ...state,
         isError: true,
         totalErrors: newErrorCount,
-        lastSignalReceived: "error",
+        lastSignalReceived: 'error',
         signalStartTimes: [...state.signalStartTimes, action.startTime],
-        signalEndTimes: [...state.signalEndTimes, action.endTime]
+        signalEndTimes: [...state.signalEndTimes, action.endTime],
       };
-    case "error":
+    case 'error':
       return {
         ...state,
         isError: true,
         totalErrors: newErrorCount,
-        lastSignalReceived: "error",
+        lastSignalReceived: 'error',
       };
-    case "dot":
+    case 'dot':
       return {
         ...state,
-        lastSignalReceived: "dot",
+        lastSignalReceived: 'dot',
         signalStartTimes: [...state.signalStartTimes, action.startTime],
-        signalEndTimes: [...state.signalEndTimes, action.endTime]
+        signalEndTimes: [...state.signalEndTimes, action.endTime],
       };
-    case "dash":
+    case 'dash':
       return {
         ...state,
-        lastSignalReceived: "dash",
+        lastSignalReceived: 'dash',
         signalStartTimes: [...state.signalStartTimes, action.startTime],
-        signalEndTimes: [...state.signalEndTimes, action.endTime]
+        signalEndTimes: [...state.signalEndTimes, action.endTime],
       };
-    case "resetSignal":
+    case 'resetSignal':
       return {
         ...state,
-        lastSignalReceived: "",
-      }
-    case "resetMessage":
+        lastSignalReceived: '',
+      };
+    case 'resetMessage':
       return {
         userSubmittedMessage: action.payload,
         signalStartTimes: [],
         signalEndTimes: [],
-        lastSignalReceived: "",
+        lastSignalReceived: '',
         isError: false,
         totalErrors: 0,
         isComplete: false,
-      }
-    case "complete":
+      };
+    case 'complete':
       return {
         ...state,
         isComplete: true,
-      }
+      };
     default:
-      throw new Error("Unknown signal");
+      throw new Error('Unknown signal');
   }
 };
 
