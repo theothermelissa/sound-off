3e769d780ee65b4d26a8d58c93a2182f3852d0f9
Melissa Morgan <melissapearlmorgan@gmail.com>
2020-02-12 09:48:32 -0500

    wip: programmatically create and position code signals in canvas

--- src/components/CanvasMaker.jsx
+++ src/components/CanvasMaker.jsx
@@ -1,31 +1,63 @@
 import React, { useRef, useEffect } from 'react';
-import sailboat from '../assets/sailboat.png';
-// import dot from '../assets/dot.svg';
+import dot from '../assets/dot.svg';
+import codeTranslationKey from '../assets/codeTranslationKey';
 
 const CanvasMaker = () => {
+  const char = 'm';
   const img = useRef(null);
   const canvasRef = useRef(null);
 
+  const dashW = 9;
+  const dotD = 5;
+  const dashH = 5;
+  const buffer = 2;
+
+  const letter = codeTranslationKey[char];
+
+  const totalSequenceLength = letter.sequence.length;
+  const canvasW = 90;
+  const canvasH = 90;
+
+  const combinedCodeSignalWidths = () => {
+    let total = letter.sequence
+      .reduce((sum, codeSignal) => ((codeSignal.id === 'dot')
+        ? (sum + dotD)
+        : (sum + dashW)), 0);
+    total += (totalSequenceLength - 1) * buffer;
+    return total;
+  };
+
+  const startPoint = (canvasW - combinedCodeSignalWidths()) / 2;
+
+  console.log('combinedCodeSignalWidths: ', combinedCodeSignalWidths());
+  console.log('startPoint: ', startPoint);
+
+  // const sequenceBoxWidth = (totalSequenceLength - 1) * buffer + (combinedCodeSignalWidths);
+
   useEffect(() => {
     const canvas = canvasRef.current;
-    const image = img.current;
+    // const image = img.current;
     const context = canvas.getContext('2d');
 
-    image.onload = () => {
-      context.drawImage(image, 0, 0, 400, 400);
-      context.font = '10px Courier';
-      context.fillStyle = '#FFFFFF';
-      context.fillText('The Why Knot (statistically)', 200, 75);
-    };
+    for (let i = 0, x = startPoint; i < totalSequenceLength; i += 1) {
+      context.fillStyle = '#CCC4BC';
+      if (letter.sequence[i].id === 'dot') {
+        context.fillRect(x, 60, dotD, dotD);
+        x = x + buffer + dotD;
+      } if (letter.sequence[i].id === 'dash') {
+        context.fillRect(x, 60, dashW, dashH);
+        x = x + buffer + dashW;
+      }
+    }
 
     // context.fillStyle = '#A68823';
     // context.fillRect(10, 10, 9, 5);
-  }, []);
+  });
 
   return (
     <div>
-      <canvas ref={canvasRef} width={400} height={400} style={{ border: '2px solid' }} />
-      <img alt="sailboat" ref={img} src={sailboat} className="hidden" />
+      <canvas ref={canvasRef} width={canvasW} height={canvasH} style={{ border: '2px solid' }} />
+      <img alt="dot" ref={img} src={dot} className="hidden" />
     </div>
   );
 };
