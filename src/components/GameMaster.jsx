2c4c5b28b5e52cf39c39e18500b30904f7366977
Melissa Morgan <melissapearlmorgan@gmail.com>
2020-03-19 17:25:00 -0400

    working: update console plug

--- src/components/GameMaster.jsx
+++ src/components/GameMaster.jsx
@@ -37,7 +37,7 @@ const GameMaster = () => {
   }, [gameState.userSubmittedMessage]);
 
   useEffect(() => {
-    console.log('*****', '\n', 'NEED A REACT DEVELOPER?', '\n', "freelance | contract-for-hire | paid internship ", '\n', "github: @theothermelissa", '\n', "linkedin @melissa-p-morgan", '\n', "*****");
+    console.log('*****', '\n', 'NEED A REACT DEVELOPER?', '\n', "freelance | contract-for-hire | paid internship ", '\n', "github: @theothermelissa", '\n', "linkedin: @melissa-p-morgan", '\n', "*****");
   }, []);
 
   useEffect(() => {
